ed08d2ae175fb291f88affe785ca1a6eb4a651e7
aipusulaofficial-cyber <aipusulaofficial@gmail.com>
2026-09-25 15:41:12 +0300

    test(load): exercise domain endpoint

--- loadtests/locustfile.py
+++ loadtests/locustfile.py
@@ -1,5 +1,6 @@
 from locust import HttpUser,task,between
 class APIUser(HttpUser):
- wait_time=between(.1,.5)
- @task
- def health(self):self.client.get("/health/live",name="health")
+    wait_time=between(.1,.5)
+    @task
+    def domain(self):
+        self.client.post("/v1/gateway",json={"key":"load","payload":{"text":"load","query":"load","version":"1","total":1,"successes":1,"target":.99}},name="/v1/gateway")
